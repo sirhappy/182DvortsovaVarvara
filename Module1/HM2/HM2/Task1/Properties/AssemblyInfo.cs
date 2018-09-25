a1be7747bdd33ea60e914bae3f6ce71192803a3a
Варвара Дворцова <vvdvortsova@edu.hse.ru>
2018-09-25 16:49:10 +0300

    новые папки

--- Module1/HM2/HM2/Task1/Properties/AssemblyInfo.cs
+++ Module1/HM2/HM2/Task1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Task1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Task1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8da53f3b-2b8d-4d07-9b37-8fbe572bf93b")]
+[assembly: Guid("6581ef56-23b6-4cd6-b799-7dea16e37661")]
 
 // Version information for an assembly consists of the following four values:
 //
