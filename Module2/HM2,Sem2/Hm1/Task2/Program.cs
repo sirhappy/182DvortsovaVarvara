13f1ed8f0d0791dd09efc3c50faecbc3ca68439d
Варвара Дворцова <vvdvortsova@edu.hse.ru>
2018-11-12 23:22:45 +0300

    Task2

---
+++ Module2/HM2,Sem2/Hm1/Task2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+	class Point
+	{
+
+
+		public double X { get; set; }
+		public double Y { get; set; }
+
+		public double Ro
+		{
+			get
+			{
+				return Math.Sqrt(X * X + Y * Y);
+			}
+		}
+
+		public double Fi
+		{
+			get
+			{
+				if (X > 0 & Y > 0)
+				{
+					return Math.Atan(Y / X);
+				}
+				else if (X > 0 & Y < 0)
+				{
+					return Math.Atan(Y / X) + 2 * Math.PI;
+				}
+				else if (X < 0)
+				{
+					return Math.Atan(Y / X) + Math.PI;
+				}
+				else if(X == 0& Y > 0)
+				{
+					return Math.PI / 2;
+				}
+				else if (X == 0 & Y < 0)
+				{
+					return Math.PI*3 / 2;
+					
+				}
+				else
+				{
+					return 0;
+				}
+			}
+		}
+	
+		public double F { get; }
+		public Point() : this(0, 0) { }
+		public Point(double y ,double x)
+			{
+				Y = y;
+				X = x;
+			}
+		public string GetInfo
+		{
+			// СВОЙСТВО
+			get
+			{
+				string maket = "X = {0:F2}; Y = {1:F2}; Ro = {2:F2}; Fi = {3:F2} ";
+				return string.Format(maket, X, Y, Ro, Fi);
+			}
+
+		}
+	}
+	class Program
+	{
+		static double EnterDouble(string message)
+		{
+			double res;
+			bool isCorrect = false;
+			do {
+
+				Console.Write(message);
+				while(!double.TryParse(Console.ReadLine(),out res) || res > 1000)
+				{
+					Console.Error.WriteLine("Error");
+					Console.Write(message);
+
+				}
+
+				isCorrect = true;
+			} while (!isCorrect);
+			return res;
+		}
+		static void Main(string[] args)
+		{
+			
+			Point a = new Point(10,10);
+			Console.WriteLine(a.GetInfo);
+			Point b = new Point(30,30);
+			Console.WriteLine(b.GetInfo);
+			
+			Point c = new Point();
+			double x, y;
+			do
+			{
+				
+				x = EnterDouble("X = ");
+				y  = EnterDouble("Y = ");
+				c.X = x; c.Y = y;
+				// #TODO3: следующий слайд
+			} while (x != 0 | y != 0);
+			
+
+
+
+			
+			
+			Console.ReadKey();
+
+
+		}
+	}
+}
---
+++ Module2/HM2,Sem2/Hm1/test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+	
+	
+		class TimePeriod
+		{
+			private double seconds;//поле
+
+			public double Hours//свойство для часов
+			{
+				get { return seconds / 3600; }
+				set
+				{
+					if (value < 0 || value > 24)
+						throw new ArgumentOutOfRangeException(
+							  $"{nameof(value)} must be between 0 and 24.");
+
+					seconds = value * 3600;
+				}
+			}
+			}
+		class Program
+		{
+		static void Main(string[] args)
+		{
+			TimePeriod t = new TimePeriod();
+			t.Hours = 23;
+			double min = t.Hours;
+			Console.WriteLine($"{t.Hours}");
+				Console.ReadKey();
+			}
+		}
+	}
